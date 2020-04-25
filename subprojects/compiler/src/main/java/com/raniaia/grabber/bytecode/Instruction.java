9bea59edf03ce67fd08cd9487a9577e7f7d85fe7
2B键盘 <ltsloveyellow@qq.com>
2020-04-25 18:49:48 +0800

    更新指令集

--- subprojects/compiler/src/main/java/com/raniaia/grabber/bytecode/Instruction.java
+++ subprojects/compiler/src/main/java/com/raniaia/grabber/bytecode/Instruction.java
@@ -26,10 +26,315 @@ package com.raniaia.grabber.bytecode;
 /**
  * 中间代码，Grabber指令集
  *
+ * ICONST           -- ICONST代表int类型
+ * UICONST          -- UICONST代表无符号int类型
+ * LCONST           -- LCONST代表long类型
+ * ULCONST          -- ULCONST代表无符号long类型
+ * CHCONST          -- CHCONST代表char类型
+ *
  * @author tiansheng
  */
+@SuppressWarnings("all")
 public enum Instruction {
 
+    /**
+     * 什么也不做
+     */
+    NOP,
 
+    /**
+     * 将int类型的0推送至栈顶
+     */
+    ICONST_0,
+
+    /**
+     * 将int类型的1推送至栈顶
+     */
+    ICONST_1,
+
+    /**
+     * 将int类型的2推送至栈顶
+     */
+    ICONST_2,
+
+    /**
+     * 将int类型的3推送至栈顶
+     */
+    ICONST_3,
+
+    /**
+     * 将int类型的4推送至栈顶
+     */
+    ICONST_4,
+
+    /**
+     * 将int类型的5推送至栈顶
+     */
+    ICONST_5,
+
+    /**
+     * 将int类型的6推送至栈顶
+     */
+    ICONST_6,
+
+    /**
+     * 将int类型的7推送至栈顶
+     */
+    ICONST_7,
+
+    /**
+     * 将int类型的8推送至栈顶
+     */
+    ICONST_8,
+
+    /**
+     * 将int类型的9推送至栈顶
+     */
+    ICONST_9,
+
+    /**
+     * 将无符号int类型的0推送至栈顶
+     */
+    UICONST_0,
+
+    /**
+     * 将无符号int类型的1推送至栈顶
+     */
+    UICONST_1,
+
+    /**
+     * 将无符号int类型的2推送至栈顶
+     */
+    UICONST_2,
+
+    /**
+     * 将无符号int类型的3推送至栈顶
+     */
+    UICONST_3,
+
+    /**
+     * 将无符号int类型的4推送至栈顶
+     */
+    UICONST_4,
+
+    /**
+     * 将无符号int类型的5推送至栈顶
+     */
+    UICONST_5,
+
+    /**
+     * 将无符号int类型的6推送至栈顶
+     */
+    UICONST_6,
+
+    /**
+     * 将无符号int类型的7推送至栈顶
+     */
+    UICONST_7,
+
+    /**
+     * 将无符号int类型的8推送至栈顶
+     */
+    UICONST_8,
+
+    /**
+     * 将无符号int类型的9推送至栈顶
+     */
+    UICONST_9,
+
+    /**
+     * 将long类型的0推送至栈顶
+     */
+    LCONST_0,
+
+    /**
+     * 将long类型的1推送至栈顶
+     */
+    LCONST_1,
+
+    /**
+     * 将long类型的2推送至栈顶
+     */
+    LCONST_2,
+
+    /**
+     * 将long类型的3推送至栈顶
+     */
+    LCONST_3,
+
+    /**
+     * 将long类型的4推送至栈顶
+     */
+    LCONST_4,
+
+    /**
+     * 将long类型的5推送至栈顶
+     */
+    LCONST_5,
+
+    /**
+     * 将long类型的6推送至栈顶
+     */
+    LCONST_6,
+
+    /**
+     * 将long类型的7推送至栈顶
+     */
+    LCONST_7,
+
+    /**
+     * 将long类型的8推送至栈顶
+     */
+    LCONST_8,
+
+    /**
+     * 将long类型的9推送至栈顶
+     */
+    LCONST_9,
+
+    /**
+     * 将无符号long类型的0推送至栈顶
+     */
+    ULCONST_0,
+
+    /**
+     * 将无符号long类型的1推送至栈顶
+     */
+    ULCONST_1,
+
+    /**
+     * 将无符号long类型的2推送至栈顶
+     */
+    ULCONST_2,
+
+    /**
+     * 将无符号long类型的3推送至栈顶
+     */
+    ULCONST_3,
+
+    /**
+     * 将无符号long类型的4推送至栈顶
+     */
+    ULCONST_4,
+
+    /**
+     * 将无符号long类型的5推送至栈顶
+     */
+    ULCONST_5,
+
+    /**
+     * 将无符号long类型的6推送至栈顶
+     */
+    ULCONST_6,
+
+    /**
+     * 将无符号long类型的7推送至栈顶
+     */
+    ULCONST_7,
+
+    /**
+     * 将无符号long类型的8推送至栈顶
+     */
+    ULCONST_8,
+
+    /**
+     * 将无符号long类型的9推送至栈顶
+     */
+    ULCONST_9,
+
+    /**
+     * 将一个byte类型的值推送至栈顶
+     */
+    BYCONST,
+
+    /**
+     * 将一个布尔类型的true推送至栈顶
+     */
+    BOOLCONST_TRUE,
+
+    /**
+     * 将一个布尔类型的false推送至栈顶
+     */
+    BOOLCONST_FALSE,
+
+    /**
+     * 执行int类型的加法
+     */
+    IADD,
+
+    /**
+     * 执行int类型的减法
+     */
+    ISUB,
+
+    /**
+     * 执行int的类型的乘法
+     */
+    IMUL,
+
+    /**
+     * 执行int类型的除法
+     */
+    IDIV,
+
+    /**
+     * 执行无符号int类型的加法
+     */
+    UIADD,
+
+    /**
+     * 执行无符号int类型的减法
+     */
+    UISUB,
+
+    /**
+     * 执行无符号int的类型的乘法
+     */
+    UIMUL,
+
+    /**
+     * 执行无符号int类型的除法
+     */
+    UIDIV,
+
+    /**
+     * 执行long类型的加法
+     */
+    LADD,
+
+    /**
+     * 执行long类型的减法
+     */
+    LSUB,
+
+    /**
+     * 执行long的类型的乘法
+     */
+    LMUL,
+
+    /**
+     * 执行long类型的除法
+     */
+    LDIV,
+
+    /**
+     * 执行无符号long类型的加法
+     */
+    ULADD,
+
+    /**
+     * 执行无符号long类型的减法
+     */
+    ULSUB,
+
+    /**
+     * 执行无符号long类型的乘法
+     */
+    ULMUL,
+
+    /**
+     * 执行无符号long类型的除法
+     */
+    ULDIV,
 
 }
